a46b2eac5a299893130ba5e5cb4451605cc04179
Absdag <Absdag@github.com>
2022-08-11 23:10:59 -0500

    Creada documentacion (con Eclipse)

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"co.udistrital.model","l":"ArchivosPokemon"}];updateSearchResults();
--- src/co/udistrital/model/ArchivosPokemon.java
+++ src/co/udistrital/model/ArchivosPokemon.java
@@ -10,6 +10,9 @@ import com.fasterxml.jackson.core.exc.StreamWriteException;
 import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+/**
+ * Esta clase maneja el inventario, lectura y escritura de archvios manejados en JSON
+ */
 public class ArchivosPokemon {
 	
 	private int largoLista;
@@ -17,13 +20,24 @@ public class ArchivosPokemon {
 	private Pokemon listaTemp[];
 	private ObjectMapper objectMapper;
 	
+	/**
+	 * Constructor base de ArchivosPokemon<br>
+	 * <b>pre</b> Ningno, la clase funciona sin condicion previa<br>
+	 * <b>post</b> Tiene configurado ahora<br>
+	 */
 	public ArchivosPokemon() {
+		
 		lista = new Pokemon[15];
 		listaTemp = new Pokemon[15];
 		objectMapper = new ObjectMapper();
 		leerArchivo();
 	}
 	
+	/**
+	 * Lectura del archivo, encargado de leer el .JSON que se manejara en el programa<br>
+	 * <b>Pre</b> N/A<br>
+	 * <b>Post</b> Leido el pokemonArchivo.json, y de no existir, creada la base del mismo
+	 */
 	public void leerArchivo() {
 		try {
 			lista = objectMapper.readerFor(Pokemon[].class).readValue(new File("pokemonArchivo.json"));
@@ -48,6 +62,11 @@ public class ArchivosPokemon {
 //		}
 //	Asi son guardados en el archivo, el poder debe ser calculado antes de guardar el pokemon
 	
+	/**
+	 * Maneja el guardado del archivo "pokemonArchivo.json"<br>
+	 * <b>Pre</b> Existencia del archivo<br>
+	 * <b>Post</b> Modificacion del archivo con los datos del programa<br>
+	 */
 	public void guardarArchivo() {
 		try {
 			objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File("pokemonArchivo.json"), lista);
@@ -62,15 +81,12 @@ public class ArchivosPokemon {
 			e.printStackTrace();
 		}
 	}
-	
-	public int cantidadPokemonEnArchivo() {
-		largoLista = lista.length;
-		return largoLista;
-	}
-	public Pokemon[] obtenerListaPokemon() {
-		return lista;
-	}
-	
+	/**
+	 * Eliminacion de un pokemon especificio
+	 * <b>Pre</b> Existencia del pokemon en la base de datos
+	 * <b>Post</b> Eliminacion del pokemon de la base de datos e invocacion interna de {@link #guardarArchivo()}
+	 * @param id Numero del pokemon en la lista, id!=null id &#x3C; 15
+	 */
 	public void eliminarPokemonArchivo(int id) {
 		if(id>0){
 			System.arraycopy(lista, 0, listaTemp, 0, id);
@@ -80,4 +96,14 @@ public class ArchivosPokemon {
 		}
 		lista = listaTemp.clone();
 	}
+	
+	public int cantidadPokemonEnArchivo() {
+		largoLista = lista.length;
+		return largoLista;
+	}
+	public Pokemon[] obtenerListaPokemon() {
+		return lista;
+	}
+	
+
 }
